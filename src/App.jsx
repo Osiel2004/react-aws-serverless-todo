c04a7cb15b38474f3cb54527876e83e886193a46
Osiel2004 <al22020345@itsa.edu.mx>
2026-03-22 19:47:41 -0600

    Inicialización del proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi Lista de Tareas Serverless</h1>
+      <p>Cargando vista pública...</p>
+    </div>
+  )
+}
+
+export default App;
